0ce68801fb5902773177275aa7c3c993a012105b
lee enisuoh <4120416+leecarl11@users.noreply.github.com>
2022-06-28 13:01:17 +0100

    a handy message here

--- python_notes/recap.py
+++ python_notes/recap.py
@@ -40,19 +40,91 @@
 #print("Hello my name is {} and i am {} years old". format(my_name,my_age))
 #print(f"Hello my name is {my_name} and i am {my_age} years old")
 
-print(1+2)
-print(3-2)
-print(3*3)
+#print(1+2)
+#print(3-2)
+#print(3*3)
 #exponential
-print(3*3*3)
-print(6/2)
+#print(3*3*3)
+#print(6/2)
 #modulus/remainder
-print(15%3)
+#print(15%3)
 
-balance=100
-print(balance)
-amount=200
+#balance=100
+#print(balance)
+#amount=200
 
-balance=amount+balance
-balance +=amount
-print(balance)
+#balance=amount+balance
+#balance +=amount
+#print(balance)
+
+#answer=input("What is your name?")
+#print(answer)
+
+#music = "classical"
+
+#if music == "classical":
+#    print("Oh no its classical")
+#elif music == "no music":
+#    print("Put the radio on")
+#elif music == "my chemical romance":
+#    print("Debbie, stop crying")    
+#else:
+#    print("turn it off")   
+#print("That was an if statement")     
+
+#print(10%3==0)
+
+# num=10
+# num2=20
+
+# if num > num2:
+#     print(f"{num} is bigger")
+# elif num2 > num:
+#     print(f"{num2} is bigger")
+# else:
+#     print("Both are equal")
+
+# place = "MCR"
+# weather = "Cloudy"
+
+# if place == "MCR" and weather == "Sunny":
+#     print("Check again")
+# elif place == "MCR" and weather == "Cloudy":
+#     print("What is isn't raining")
+# else:
+#     print("obvious")
+    
+# day = "Friday"
+# bank_hol = False
+
+# if day == "Saturday" or day=="Sunday" or bank_hol:
+#     print("A day off")
+#     #print("It's the weekend!")
+# else:
+#     print("Off to innovate we go")
+
+# def light_switch():
+#     print("Switching the lights")
+    
+# light_switch()
+
+# def cash_with (amount, accnum):
+#     print(f"from you account you have withdrawn {amount} from account {accnum}")
+# cash_with(300, 12345678)
+
+# fav_songs = ["the foundation of decay - my chemical romance ", "Pandemonium - killing joke", "Jigsaw falling into Place Radiohead"]
+
+# print(fav_songs)
+
+# fav_games = ["cypherpunk", "watch dog", "grand theft auto"]
+
+# print(fav_games)
+
+# fav_songs[1] = "Adam's song blink 182"
+# print(fav_songs)
+# print(len(fav_songs))
+# fav_songs.append("Army of me - bjork")
+# print(fav_songs)
+# #pop will get rid off an item from a list
+# fav_songs.pop(2)
+# print(fav_songs)
