7de93c0ff07c5ce93e621e7cafb308731bc994cd
lee enisuoh <4120416+leecarl11@users.noreply.github.com>
2022-06-28 11:37:42 +0100

    a good message here

--- python_notes/recap.py
+++ python_notes/recap.py
@@ -1,8 +1,58 @@
-print("this is my file")
-message = "how are you"
-print (message)
-print(1234 + 20)
-print(12.34 + 10)
-print(True)
-print(False)
-print(None)
+#import random
+#print("this is my file")
+#message = "how are you"
+
+#greeting = "Hello everyone"
+#print (message)
+#print(1234 + 20)
+#print(12.34 + 10)
+#print(True)
+#print(False)
+#print(None)
+
+#print(len(greeting))
+#print(greeting[1])
+#print(greeting[-1])
+#print(greeting.upper())
+#print(greeting)
+
+#print("HELLO".lower())
+#print("hello EVERYONE. This is innovate".capitalize())
+#print("This quick brown fox fox fox".count("fox "))
+#print("TheT quick brown fox".find("T"))
+#print("The quick brown fox".replace("fox", "frog"))
+#print(len("The quick brown fox            "))
+
+#print(random.random())
+
+#print(random.uniform(1,10))
+
+#print(random.randint(1,10))
+
+#my_name="Debbie"
+#my_age=40
+#student=False
+
+#print(my_name,my_age,student)
+#print("Hello my name is", my_name)
+#print("Hello my name " + my_name)
+
+#print("Hello my name is {} and i am {} years old". format(my_name,my_age))
+#print(f"Hello my name is {my_name} and i am {my_age} years old")
+
+print(1+2)
+print(3-2)
+print(3*3)
+#exponential
+print(3*3*3)
+print(6/2)
+#modulus/remainder
+print(15%3)
+
+balance=100
+print(balance)
+amount=200
+
+balance=amount+balance
+balance +=amount
+print(balance)
