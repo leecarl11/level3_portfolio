dd1d8ef5217c8c2aaf9e9bc121c1d2f95288d2fe
lee enisuoh <4120416+leecarl11@users.noreply.github.com>
2022-07-09 22:49:28 +0100

    a good message here

--- python-notes/dictionary-notes.py
+++ python-notes/dictionary-notes.py
@@ -1,5 +1,6 @@
 print("this is dictionaries")
-#dictionaries store data for us in a really specific way, just like actual dictionaries you can look things up, but not in alphabetic order,they are more like lists they store multiple values to one variable we denote a dictionary by using curly brackets{}these are used to define our dictionaries,dictionary stores key:value pairs.Basically, they allow you to give each element a name  
+#dictionaries store data for us in a really specific way, just like actual dictionaries you can look things up, but not in alphabetic order,they are more like lists they store multiple values to one variable we denote a dictionary by using curly brackets{}these are used to define our dictionaries,dictionary stores key:value pairs.Basically, they allow you to give each element a name, we will be using 2 bits of information the key and the value
+#this is a dictionary, 3 items there in our dictionary made up of key:value,pairs, they all have key words and value. each key in the dictionary must be unique, they cannot have 2 named keys, it will just read whatever one it reads last if that's the case. no key duplicates are allowed,what duplicate values are allowed
 my_cat = {
     "name": "Salem",
     "colour": "Black",
