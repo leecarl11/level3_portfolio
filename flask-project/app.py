c35bdc1b1c5a6bc20034e9ac224008168f0e71fe
lee enisuoh <4120416+leecarl11@users.noreply.github.com>
2022-07-11 06:11:40 +0100

    a good message here

--- flask-project/app.py
+++ flask-project/app.py
@@ -14,3 +14,5 @@ def Debbie():
 #this if statement is the last thing on our app - it allows the initialisation of the app. Debugis set to true, and we pick a port to run on. 5000 is default and 8000 is linked to the debugger
 if __name__=="__main__":
     app.run(debug=True, port=8000)
+    
+#we have just been setting up different functions to return bits of informations for us based on what we type in the browser. flask apps need a very specific file structure to work from,this is how they must have been written to work, if we want to actually make WEBPAGES rather than just return random strings, if we want a page to come back us it must be in a specific file structure(create a templates folders in the flask-project folder and also create a static folder inside the flask-project folder also)if we are wanting our flask to manage WEBPAGES which we are in this case we are making a web application we must have this file structure, WEBPAGES and html files will go in templates folder , static files like images css, javascript will all go in the static folder, we must have this structure for flask app to work properly
