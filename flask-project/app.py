848b7010edc7513699a546a7f63d18fd22162971
lee enisuoh <4120416+leecarl11@users.noreply.github.com>
2022-07-11 05:06:06 +0100

    a good message here

--- flask-project/app.py
+++ flask-project/app.py
@@ -2,15 +2,15 @@ from flask import Flask
 #creating an instance of a Flask class called app
 app = Flask(__name__)
 
-#using the route() method on our app. this builds endpoints - i.e it tells our app how to run depending on the browser. for example, if my user visits www.domain.com/ - the index() function will then run
+#using the route() method on our app. this builds endpoints - i.e it tells our app how to run depending on the browser. for example, if my user visits www.domain.com/ - the index() function will then run, the job of the index function is to return hello world
 @app.route("/")
 def index():
     return "<h1>Hello World!</h1>"
 
-#this if statement is the last thing on our app - it allows the initialisation of the app. Debugis set to true, and we pick a port to run on. 5000 is default and 8000 is linked to the debugger
 @app.route("/Debbie")
 def Debbie():
-    return "Debbie\'s page"
+    return "<h1>This is Debbie\'s business app</h1>"
 
+#this if statement is the last thing on our app - it allows the initialisation of the app. Debugis set to true, and we pick a port to run on. 5000 is default and 8000 is linked to the debugger
 if __name__=="__main__":
     app.run(debug=True, port=8000)
